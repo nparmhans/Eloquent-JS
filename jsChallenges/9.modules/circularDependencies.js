b086bcec7b7e29fe84b7c6c5bae4a5861fa81e43
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 03:19:01 -0400

    added hints

--- jsChallenges/9.modules/circularDependencies.js
+++ jsChallenges/9.modules/circularDependencies.js
@@ -1,2 +1,7 @@
 // HINTS...
+
+// The trick is to add the exports object created for a module to require's cache before actually running the module. This means the module will not yet have had a chance to override module.exports, so we do not know whether it may want to export some other value. After loading, the cache object is overridden with module.exports, which may be a different value.
+
+// But if, in the course of loading the module, a second module is loaded that asks for the first module, its default exports object, likely still empty at this point, will be in the cache, and the second module will receive a reference to it. If it doesn’t try to do anything with the object until the first module has finished loading, things will work.
+
 // code below here:
