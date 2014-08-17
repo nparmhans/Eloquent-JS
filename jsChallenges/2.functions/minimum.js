a2cf2bb264d0c3b3dd458ee8b274fe8ec42fdecb
Stephen Estrada <stcrestrada@gmail.com>
2014-08-16 23:22:57 -0400

    added hints to js files

---
+++ jsChallenges/2.functions/minimum.js
@@ -0,0 +1,7 @@
+// HINTS...
+
+// If you have trouble putting braces and parentheses in the right place to get a valid function definition, start by copying one of the examples in this chapter and modifying it.
+
+// A function may contain multiple return statements.
+
+// Code goes below this line:
