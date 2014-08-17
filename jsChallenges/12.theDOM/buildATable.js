10983a86064696ef5f54e4cc48ccf77974c194e9
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 04:04:51 -0400

    added hints

--- jsChallenges/12.theDOM/buildATable.js
+++ jsChallenges/12.theDOM/buildATable.js
@@ -1,5 +1,10 @@
 // HINTS...
 
+// Use document.createElement to create new element nodes, document.createTextNode to create text nodes, and the appendChild method to put nodes into other nodes.
+
+// You should loop over the key names once to fill in the top row, and then again for each object in the array to construct the data rows.
+
+// Don’t forget to return the enclosing <table> element at the end of the function.
 
 
 
