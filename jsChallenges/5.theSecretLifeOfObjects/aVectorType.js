bffb20da55e41d58b5286b508792f067a5656809
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 01:25:30 -0400

    added hints to all challenges

--- jsChallenges/5.theSecretLifeOfObjects/aVectorType.js
+++ jsChallenges/5.theSecretLifeOfObjects/aVectorType.js
@@ -1,5 +1,7 @@
 // HINTS...
 
+// Your solution can follow the pattern of the Rabbit constructor from this chapter quite closely.
 
+// Adding a getter property to the constructor can be done with the Object.defineProperty function. To compute the distance from (0, 0) to (x, y), you can use the Pythagorean theorem, which says that the square of the distance we are looking for is equal to the square of the x coordinate plus the square of the y coordinate. Thus, √(x2 + y2) is the number you want, and Math.sqrt is the way you compute a square root in JavaScript.
 
 // code below here:
