3d1fafeca7e0b2d4a852756c8693eded53ad8b3f
Stephen Estrada <stcrestrada@gmail.com>
2014-08-16 23:21:29 -0400

    updated recursion file

---
+++ jsChallenges/2.functions/recursion.js
@@ -0,0 +1,6 @@
+// HINTS...
+// Your function will likely look somewhat similar to the inner find function in the recursive findSolution example in this chapter, with an if/else if/else chain that tests which of the three cases applies. The final else, corresponding to the third case, makes the recursive call. Each of the branches should contain a return statement or in some other way arrange for a specific value to be returned.
+
+// When given a negative number, the function will recurse again and again, passing itself an ever more negative number, thus getting further and further away from returning a result. It will eventually run out of stack space and abort.
+
+// Code goes below this line:
