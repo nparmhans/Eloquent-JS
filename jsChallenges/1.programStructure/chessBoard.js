bd2d6fe0e3cb71cace9e74e28014c7d3594ae884
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 10:43:01 -0400

    completed chapters 1

--- jsChallenges/1.programStructure/chessBoard.js
+++ jsChallenges/1.programStructure/chessBoard.js
@@ -11,6 +11,28 @@
 // Terminating a line by adding a newline character happens after the line has been built up, so do this after the inner loop but inside of the outer loop.
 
 // code below here:
+var size = 8;
+var board = ""
 
+for (var y=0; y < size; y++ ){
+	for (var x=0; x < size; x++){
+		if((x+y) % 2 === 0){
+			board += "#";
+		}
+		else {
+			board += " ";
+		}
+	board += "\n"
+	}
+console.log(board);
 
+}
 
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
--- jsChallenges/1.programStructure/fizzBuzz.js
+++ jsChallenges/1.programStructure/fizzBuzz.js
@@ -17,3 +17,13 @@ for (var i=1; i <= 100; i++){
 		console.log("FizzBuzz");
 	}
 }
+
+
+// for (var n= 1; n <= 100; n++){
+// 	var output = "";
+// 	if (n % 3 === 0){
+// 		output += "Fizz";
+// 	}if (n % 5 === 0){
+// 		output += "Buzz";
+// 	}
+}
