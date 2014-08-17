ee6632ad86c69a5cc944ef44adf1b52e13dc1983
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 02:55:28 -0400

    added hints to qs.js

--- jsChallenges/8.regularExpressions/quotingStyle.js
+++ jsChallenges/8.regularExpressions/quotingStyle.js
@@ -1,3 +1,9 @@
 // HINTS...
 
+// The most obvious solution is to only replace quotes with a nonword character on at least one side. Something like /\W'|'\W/. But you also have to take the start and end of the line into account.
+
+// In addition, you must ensure that the replacement also includes the characters that were matched by the \W pattern so that those are not dropped. This can be done by wrapping them in parentheses and including their groups in the replacement string ($1, $2). Groups that are not matched will be replaced by nothing.
+
+
+
 // code below here:
