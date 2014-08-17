5e4d4e7be05e966b566c632c5a12330cbe2805f2
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 02:32:22 -0400

    lockbox.js hints

---
+++ jsChallenges/7.bugsAndErrorHandling/lockedBox.js
@@ -0,0 +1,7 @@
+// HINTS...
+
+// This exercise calls for a finally block, as you probably guessed. Your function should first unlock the box and then call the argument function from inside a try body. The finally block after it should lock the box again.
+
+// To make sure we don’t lock the box when it wasn’t already locked, check its lock at the start of the function and unlock and lock it only when it started out locked.
+
+// code below here:
