6aab942755174f3494bc36489a0bc675bd9a3064
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 03:34:57 -0400

    Added

--- jsChallenges/10.projectAProgrammingLanguage/fixingAScope.js
+++ jsChallenges/10.projectAProgrammingLanguage/fixingAScope.js
@@ -1,2 +1,7 @@
 // HINTS...
+
+// You will have to loop through one scope at a time, using Object.getPrototypeOf to go the next outer scope. For each scope, use hasOwnProperty to find out if the variable, indicated by the name property of the first argument to set, exists in that scope. If it does, set it to the result of evaluating the second argument to set, and return that value.
+
+// If the outermost scope is reached (Object.getPrototypeOf returns null) and we haven’t found the variable yet, it doesn’t exist, and an error should be thrown.
+
 // code below here:
