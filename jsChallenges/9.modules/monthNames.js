373feaaf75471c867b788512f757cdbae2a422e9
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 03:19:33 -0400

    added hints

--- jsChallenges/9.modules/monthNames.js
+++ jsChallenges/9.modules/monthNames.js
@@ -1,2 +1,5 @@
 // HINTS...
+
+// This follows the weekDay module almost exactly. A function expression, called immediately, wraps the variable that holds the array of names, along with the two functions that must be exported. The functions are put in an object, and returned. The returned interface object is stored in the month variable.
+
 // code below here:
