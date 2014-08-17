37ff0c232e074d2a77a0ffde840ba57bd94423f3
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 02:31:07 -0400

    rety.js hints added

--- jsChallenges/7.bugsAndErrorHandling/retry.js
+++ jsChallenges/7.bugsAndErrorHandling/retry.js
@@ -1,3 +1,7 @@
 // HINTS...
 
+// The call to primitiveMultiply should obviously happen in a try block. The corresponding catch block should rethrow the exception when it is not an instance of MultiplicatorUnitFailure and ensure the call is retried when it is.
+
+// To do the retrying, you can either use a loop that breaks only when a call succeeds—as in the look example earlier in this chapter—or use recursion and hope you don’t get a string of failures so long that it overflows the stack (which is a pretty safe bet).
+
 // code below here:
