f8bba6f7ef07bcc2f57fb91525ef7ad4b14ca6e5
Stephen Estrada <stcrestrada@gmail.com>
2014-08-17 01:24:54 -0400

    added hints to anther cell

--- jsChallenges/5.theSecretLifeOfObjects/anotherCell.js
+++ jsChallenges/5.theSecretLifeOfObjects/anotherCell.js
@@ -1,3 +1,7 @@
 // HINTS...
 
+// You’ll have to store all three constructor arguments in the instance object. The minWidth and minHeight methods should call through to the corresponding methods in the inner cell but ensure that no number less than the given size is returned (possibly using Math.max).
+
+// Don’t forget to add a draw method that simply forwards the call to the inner cell.
+
 // code below here:
