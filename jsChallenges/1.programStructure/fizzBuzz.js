85a4f605f51b2f738cd91e1c009a5a54c0e04f1e
Nirupa Parmhans <nparmhans@new-host.home>
2014-10-02 20:55:53 -0400

    add working array loop

--- jsChallenges/1.programStructure/fizzBuzz.js
+++ jsChallenges/1.programStructure/fizzBuzz.js
@@ -6,3 +6,8 @@
 // The second version of the program has a straightforward solution and a clever one. The simple way is to add another “branch” to precisely test the given condition. For the clever method, build up a string containing the word or words to output, and print either this word or the number if there is no word, potentially by making elegant use of the || operator.
 
 // code below here:
+
+var myArray = [1, 2, 3];
+for(var i=0; i< myArray.length; i++){
+	console.log(array[i]);
+}
